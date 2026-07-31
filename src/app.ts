81e84d523360a16706e637e970bc9581e9b90f74
audiputra19 <audiputra19@gmail.com>
2026-07-31 09:49:28 +0700

    update base url of frontend

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ import hakAksesRouter from "./routers/hakAksesRoutes";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://koperasi-next-app.vercel.app'
+    // origin: 'http://localhost:3000'
     // origin: 'https://koperasi-app-omega.vercel.app'
 }));
 
