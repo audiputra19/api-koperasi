eb3e79ea74f08e01334c5fa20e3195332d8001d2
audiputra19 <audiputra19@gmail.com>
2026-08-03 13:58:09 +0700

    fix bug timezone in database config

--- src/config/db/kopsas.ts
+++ src/config/db/kopsas.ts
@@ -4,7 +4,8 @@ const connKopsas = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME_KOPSAS
+    database: process.env.DB_NAME_KOPSAS,
+    timezone: '+07:00'
 });
 
 connKopsas.getConnection()
--- src/config/db/payroll.ts
+++ src/config/db/payroll.ts
@@ -4,7 +4,8 @@ const connPayroll = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME_PAYROLL
+    database: process.env.DB_NAME_PAYROLL,
+    timezone: '+07:00'
 });
 
 connPayroll.getConnection()
